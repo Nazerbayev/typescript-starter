443f69c530c25bfa9c5f565d5341adaa129661b5
William Bettencourt <williambet@gmail.com>
2016-02-21 21:46:59 -0500

    modifying gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,64 +3,46 @@ var gulp 		= require('gulp'),
 	replace 	= require('gulp-replace'),
 	rename 		= require('gulp-rename'),
 	sourceMaps 	= require('gulp-sourcemaps'),
-	source 		= require('vinyl-source-stream'),
-	exorcist 	= require('exorcist'),
-	browserify 	= require('browserify'),
-	debowerify 	= require('debowerify'),
-	tsify 		= require('tsify'),
-	uglifyJS 	= require('gulp-uglify'),
-	uglifyCSS 	= require('gulp-minify-css');
+    browserify 	= require('browserify'),
+    watchify 	= require('watchify'),
+    source 		= require('vinyl-source-stream'),
+    stringify 	= require('stringify'),
+    exorcist	= require('exorcist'),
+    debowerify	= require('debowerify'),
+    uglifyJS	= require('gulp-uglify'),
+    uglifyCSS	= require('gulp-minify-css'),
+    tsify 		= require('tsify');
 
 var config = {
-	bowerDir: __dirname + '/bower_components',
-	applicationDir: __dirname + "/src",
-	stylesDir: __dirname + '/src/styles',
-	publicDir: __dirname + '/public'
+	application: './src',
+	public: './public',
+    entrypoint: './src/app/app.ts',
+    bundleName: 'application.js'
+};
+ opts = {
+	basedir: config.application,
+    cache: {},
+    packageCache: {},
+    debug: true,
+    entries: config.entrypoint
 };
 
-gulp.task('compile-js', function(){
-	var bundler = browserify({
-		basedir: config.applicationDir,
-		debug: true
-		}).add(config.applicationDir + '/app.ts')
-		.plugin(tsify)
-		.transform(debowerify);
-
-	return bundler.bundle()
-			.pipe(exorcist(config.publicDir + '/application.js.map'))
-			.pipe(source('application.js'))
-			.pipe(gulp.dest(config.publicDir));
+var b = browserify(opts);
+b = watchify(b, {poll: true});
+b.plugin(tsify)
+	.transform(debowerify),
+    .on('update', bundle)
+    .on('log', function(data) {
+        console.log(data);
 });
 
-gulp.task('uglify-js', function(){
-	return gulp.src(config.publicDir + '/application.js')
-			.pipe(uglifyJS())
-			.pipe(rename('application.min.js'))
-			.pipe(gulp.dest(config.publicDir));
-});
+function bundle() {
+    return b.bundle()
+    	.pipe(exorcist(config.public + '/application.js.map'))
+        .pipe(source(config.bundleName))
+        .pipe(gulp.dest(config.public));
+}
 
-gulp.task('compile-css', function() {
-	return gulp.src(config.stylesDir + 'main.less')
-			.pipe(sourceMaps.init())
-			.pipe(less({paths: [config.stylesDir]}))
-			.pipe(replace('../fonts/glyphicons', './fonts/bootstrap/glyphicons'))
-			.pipe(rename('styles.css'))
-			.pipe(sourceMaps.write('.'))
-			.pipe(gulp.dest(config.publicDir));
-});
 
-gulp.task('uglify-css', ['compile-css'], function(){
-	return gulp.src(config.publicDir + '/style.css')
-			.pipe(uglifyCSS({
-				keepSpecialComments: 0
-			}))
-			.pipe(rename('styles.min.css'))
-			.pipe(gulp.dest(config.publicDir));
-});
+gulp.task('compile-js', bundle);
 
-gulp.task('fonts-bootstrap', function(){
-	return gulp.src(config.bowerDir + '/bootstrap/fonts/*')
-			.pipe(gulp.dest(config.publicDir + '/fonts/bootstrap'));
-});
-
-gulp.task('fonts', ['fonts-bootstrap']);
